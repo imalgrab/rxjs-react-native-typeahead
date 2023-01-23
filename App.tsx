4141f044c61521ae56f1ee34727332c5ec8236b7
imalgrab <igormalgrab@gmail.com>
2023-01-23 22:23:55 +0100

    Step 4: combine input with subject

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { interval } from 'rxjs';
+import { BehaviorSubject, interval } from 'rxjs';
 import { useSubscription } from './hooks/useSubscription';
+
+const label$ = new BehaviorSubject('');
 
 export default function App() {
   const [label, setLabel] = useState('');
+
+  useSubscription(label$.asObservable(), setLabel);
+
+  function handleTextChange(text: string) {
+    label$.next(text);
+  }
   return (
     <View style={styles.container}>
       <TextInput
-        onChangeText={setLabel}
+        onChangeText={handleTextChange}
         placeholder="Type here..."
         style={styles.input}
       />
