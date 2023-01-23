018e8b34db3a1cf71fe6f9a797db71e0d633218d
imalgrab <igormalgrab@gmail.com>
2023-01-23 22:20:42 +0100

    Step 3: add text input

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { interval } from 'rxjs';
 import { useSubscription } from './hooks/useSubscription';
 
-const value$ = interval(1000);
-
 export default function App() {
-  const [value, setValue] = useState<number>();
-
-  useSubscription(value$, setValue);
-
+  const [label, setLabel] = useState('');
   return (
     <View style={styles.container}>
-      <Text>{value}</Text>
+      <TextInput
+        onChangeText={setLabel}
+        placeholder="Type here..."
+        style={styles.input}
+      />
+      <Text>Label text: {label}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,5 +25,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: '#000',
+    width: '100%',
+    padding: 16,
+    fontSize: 16,
+    justifyContent: 'center',
+    alignContent: 'center',
   },
 });
