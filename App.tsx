2abec4fbca235043fd29327e93df3fb346b6f18b
imalgrab <igormalgrab@gmail.com>
2023-01-23 21:56:58 +0100

    Step 1: first Observable

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { interval } from 'rxjs';
+
+const value$ = interval(1000);
 
 export default function App() {
+  const [value, setValue] = useState<number>();
+
+  useEffect(() => {
+    if (value$) {
+      const subscription = value$.subscribe(setValue);
+      return () => subscription.unsubscribe();
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{value}</Text>
       <StatusBar style="auto" />
     </View>
   );
